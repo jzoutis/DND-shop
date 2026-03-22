aba21ab95189158e3686124b2548f843615e414a
JZoutis <164677742+jzoutis@users.noreply.github.com>
2026-03-21 22:47:34 -0400

    Add Vite configuration for React
    Removed error logs and added Vite configuration for React.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-error during build:
-Could not resolve entry module "index.html".
-    at getRollupError (file:///vercel/path0/node_modules/rollup/dist/es/shared/parseAst.js:402:41)
-    at error (file:///vercel/path0/node_modules/rollup/dist/es/shared/parseAst.js:398:42)
-    at ModuleLoader.loadEntryModule (file:///vercel/path0/node_modules/rollup/dist/es/shared/node-entry.js:21735:20)
-    at async Promise.all (index 0)
-Error: Command "npm run build" exited with 1
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
